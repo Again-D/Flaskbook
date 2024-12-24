3f48efdd58bbfdaa6be5971841b8f69f09c11052
Again-D <nemcn5306@gmail.com>
2024-12-24 18:30:32 +0900

    CH 7

--- apps/detector/forms.py
+++ apps/detector/forms.py
@@ -2,6 +2,14 @@ from flask_wtf.file import FileAllowed, FileField, FileRequired
 from flask_wtf.form import FlaskForm
 from wtforms.fields.simple import SubmitField
 
+
 class UploadImageForm(FlaskForm):
     # 파일 업로드에 필요한 유효성 검증을 설정한다
-    image =
+    image = FileField(
+        validators=[
+            FileRequired("이미지 파일을 지정해주세요."),
+            FileAllowed(["jpg", "png"], "이미지 파일만 업로드 가능합니다."),
+        ]  
+    )
+    submit = SubmitField("업로드")
+    
--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, current_app, render_template, send_from_directory
+# uuid를 import한다
+import uuid
+# Path를 import 한다
+from pathlib import Path
+
+from flask import (Blueprint, current_app, redirect, render_template,
+                   send_from_directory, url_for)
+# flask_login에서 login_required, current_user를 import한다
+from flask_login import current_user, login_required
 
 from apps.app import db
 from apps.crud.models import User
+from apps.detector.forms import UploadImageForm
 from apps.detector.models import UserImage
 
 # template_folder를 지정한다(static 지정은 X)
@@ -25,3 +34,31 @@ def index():
 @dt.route("/images/<path:filename>")
 def image_file(filename):
     return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
+
+@dt.route("/upload", methods=["GET", "POST"])
+# 로그인 필수로 한다
+@login_required
+def upload_image():
+    # UploadImageForm을 이용해서 검증한다
+    form = UploadImageForm()
+    if form.validate_on_submit():
+        # 업로드된 이미지 파일을 취득한다
+        file = form.image.data
+        # 파일의 파일명과 확장자를 취득하고, 파일명을 uuid로 변환한다
+        ext = Path(file.filename).suffix
+        image_uuid_file_name = str(uuid.uuid4()) + ext
+        # 이미지를 저장한다
+        image_path = Path(
+            current_app.config["UPLOAD_FOLDER"], image_uuid_file_name
+        )
+        file.save(image_path)
+        
+        # DB에 저장한다
+        user_image = UserImage(
+            user_id=current_user.id, image_path=image_uuid_file_name
+        )
+        db.session.add(user_image)
+        db.session.commit()
+        
+        return redirect(url_for("detector.index"))
+    return render_template("detector/upload.html", form=form)
